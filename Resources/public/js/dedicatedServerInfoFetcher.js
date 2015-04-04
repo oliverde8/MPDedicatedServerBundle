dd4e63fce2415565b1b289e07ec199e6b396edb6
oliverde8 <oliverde8@gmail.com>
2015-04-04 14:56:01 +0200

    Fixed js to handle new api & added some options. Added chat support and auto chat refresh as well.

--- Resources/public/js/dedicatedServerInfoFetcher.js
+++ Resources/public/js/dedicatedServerInfoFetcher.js
@@ -4,47 +4,19 @@
 
 (function ( $ ) {
 
+    var ode8_mp_server = function(element, options) {
 
-    $.fn.ode8_mp_server = function ( options ) {
-
-        var settings = $.extend({
+        var defaults = {
             login: null,
+            server_info_url: null,
+            server_maps_url: null,
+            server_chat_url: null,
             mapping : []
-        }, options);
+        }
 
-        var that = $(this);
-
-        $.ajax({
-            url : "/mp/dedicated/api/info/" + settings.login + ".json"
-        }).done(function (data){
-            console.log(data);
-
-            if (!data.error) {
-                _updateServerOption(that, data.serverOptions);
-                _updateCurrentMap(that, data.currentMap);
-
-                that.find('.mp_server__info__nbPlayers').html(data.serverPlayers.length);
-                that.find('.mp_server__info__nbSpectators').html(data.serverSpectators.length);
-
-                var playerListContainer = that.find('.mp_server__info__player_list');
-                if (playerListContainer) {
-                    _updatePlayerList(playerListContainer, data.serverPlayers);
-                }
-
-                var playerListContainer = that.find('.mp_server__info__spectator_list');
-                if (playerListContainer) {
-                    _updatePlayerList(playerListContainer, data.serverSpectators);
-                }
-
-                var mapListContainer = that.find('.mp_server__info__map_list');
-                if (playerListContainer) {
-                    _updateMapList(mapListContainer, data.maps);
-                }
-
-            } else {
-                console.error("Maniaplanet server Info can't connect to server : " + settings.login)
-            }
-        });
+        var that = $(element);
+        var plugin = this;
+        plugin.settings = {};
 
         var _updateServerOption = function(that, data) {
             for (var key in data) {
@@ -63,8 +35,8 @@
 
         var _updatePlayerList = function(that, data) {
             var inputContainer = that.find('.mp_server__info__player_container');
-            var mockUpContainer = that.find('.mp_server__info__player_mockup');
             inputContainer.html('');
+            var mockUpContainer = that.find('.mp_server__info__player_mockup');
 
 
             if (inputContainer && mockUpContainer) {
@@ -99,6 +71,7 @@
 
         var _updateMapList = function(that, data) {
             var inputContainer = that.find('.mp_server__info__map_container');
+            inputContainer.html('');
             var mockUpContainer = that.find('.mp_server__info__map_mockup');
 
             if (inputContainer && mockUpContainer) {
@@ -119,13 +92,119 @@
             }
         };
 
-        return this;
+        var _updateChatLines = function(that, data) {
+            var inputContainer = that.find('.mp_server__info__chat_container');
+            inputContainer.html('');
+            var mockUpContainer = that.find('.mp_server__info__chat_mockup');
+
+            if (inputContainer && mockUpContainer) {
+                for (var key in data) {
+                    mockUpContainer.find('.mp_server__info__chat_line').html(MPStyle.Parser.toHTML(data[key]));
+                    mockUpContainer.clone().appendTo(inputContainer);
+                }
+            }
+        };
+
+        plugin.init = function() {
+
+            plugin.settings = $.extend({}, defaults, options);
+
+            plugin.updateServerInfo();
+            plugin.updateMapList();
+            plugin.updateChatLines();
+        };
+
+        plugin.updateServerInfo = function() {
+            $.ajax({
+                url: plugin.settings.server_info_url
+            }).done(function (data) {
+
+                if (!data.error) {
+                    _updateServerOption(that, data.serverOptions);
+                    _updateCurrentMap(that, data.currentMap);
+
+                    that.find('.mp_server__info__nbPlayers').html(data.serverPlayers.length);
+                    that.find('.mp_server__info__nbSpectators').html(data.serverSpectators.length);
+
+                    var playerListContainer = that.find('.mp_server__info__player_list');
+                    if (playerListContainer) {
+                        _updatePlayerList(playerListContainer, data.serverPlayers);
+                    }
+
+                    var playerListContainer = that.find('.mp_server__info__spectator_list');
+                    if (playerListContainer) {
+                        _updatePlayerList(playerListContainer, data.serverSpectators);
+                    }
+
+                } else {
+                    console.error("Maniaplanet server Info can't connect to server : " + settings.login)
+                }
+            });
+        };
+
+        plugin.updateMapList = function() {
+            var mapListContainer = that.find('.mp_server__info__map_list');
+            if (mapListContainer && plugin.settings.server_maps_url != null) {
+                $.ajax({
+                    url: plugin.settings.server_maps_url
+                }).done(function (data) {
+                    _updateMapList(mapListContainer, data);
+                });
+            }
+        };
+
+        plugin.updateChatLines = function() {
+            var chatLinesContainer = that.find('.mp_server__info__chat');
+            if (chatLinesContainer && plugin.settings.server_chat_url != null) {
+                $.ajax({
+                    url: plugin.settings.server_chat_url
+                }).done(function (data) {
+                    _updateChatLines(chatLinesContainer, data);
+                });
+            }
+        };
+
+        var autChatUpdateTimerId = null;
+        plugin.startAutoChatUpdate = function(delay) {
+            if (autChatUpdateTimerId == null) {
+                autChatUpdateTimerId = window.setInterval(function () {
+                    plugin.updateChatLines();
+                }, delay);
+            }
+        };
+
+        plugin.stopAutoChatUpdate = function () {
+            if (autChatUpdateTimerId != null) {
+                window.clearInterval(autChatUpdateTimerId);
+            }
+        };
+
+        plugin.init();
+
     };
 
-    $( document).ready(function () {
+    // add the plugin to the jQuery.fn object
+    $.fn.ode8_mp_server = function(options) {
+        return this.each(function() {
+            if (undefined == $(this).data('ode8_mp_server')) {
+                var plugin = new ode8_mp_server(this, options);
+                $(this).data('ode8_mp_server', plugin);
+            }
+        });
+    };
+
+    $(document).ready(function () {
         $(".mp_server__info").each(function(){
             var that = $(this);
-            that.ode8_mp_server({login: that.data('server-login')});
+            that.ode8_mp_server(
+                {
+                    login: that.data('server-login'),
+                    server_info_url: that.data('server-info-url'),
+                    server_maps_url: that.data('server-maps-url'),
+                    server_chat_url: that.data('server-chat-url')
+                }
+            );
+            that.data('ode8_mp_server').startAutoChatUpdate(10000);
         });
     });
 }( jQuery ));
