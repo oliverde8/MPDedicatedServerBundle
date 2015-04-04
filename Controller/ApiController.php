3d8591f1fcff655f1524c355800d2dd98c9053d5
oliverde8 <oliverde8@gmail.com>
2015-04-04 14:53:44 +0200

    Changed api to send data in 3 differnt api calls instead of one. Changed Urls to be nicer.

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -11,12 +11,8 @@ class ApiController extends Controller
 {
     public function serverInfoAction($login)
     {
-        /** @var \Doctrine\Common\Cache\Cache $cache */
-        $cache = $this->container->get('doctrine_cache.providers.oliverde8_mp_dedicated_server__info');
-
         /** @var DedicatedServer $dedicated */
         $dedicated = $this->get('oliverde8_mp_dedicated_server.dedicated_server');
-
         $serverInfo = $dedicated->getServerInfo($login);
 
         if (!$serverInfo) {
@@ -45,7 +41,42 @@ class ApiController extends Controller
         return $response;
     }
 
+    public function serverChatLinesAction($login)
+    {
+        /** @var DedicatedServer $dedicated */
+        $dedicated = $this->get('oliverde8_mp_dedicated_server.dedicated_server');
+        $chatLines = $dedicated->getServerChatLines($login);
 
+        if (is_null($chatLines)) {
+            $chatLines = $this->serverUnAvailableError();
+        }
+
+        return $this->prepareResponse($chatLines);
+    }
+
+    public function serverMapListAction($login)
+    {
+        /** @var DedicatedServer $dedicated */
+        $dedicated = $this->get('oliverde8_mp_dedicated_server.dedicated_server');
+        $mapList = $dedicated->getMapList($login);
+
+        if (!$mapList) {
+            $mapList = $this->serverUnAvailableError();
+        }
+
+        return $this->prepareResponse($mapList);
+    }
+
+    protected function prepareResponse($data) {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setMaxAge(60);
+        $response->setPublic();
+        $response->setContent(json_encode($data));
+        $response->setLastModified(new \DateTime());
+
+        return $response;
+    }
 
     protected function serverUnAvailableError()
     {
